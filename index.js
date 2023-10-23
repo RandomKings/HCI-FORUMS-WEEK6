e675643e3f03577fc8f77ac20bf4086e280ee949
Pranav Harish Nathani <pranav.nathani@binus.ac.id>
2023-10-23 23:41:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,49 +23,40 @@ let bankBalance = 1000;
         }
 
      
-        function updateTransactionHistory(transaction) {
-            const transactionList = document.getElementById("transactionList");
-            const listItem = document.createElement("li");
-            listItem.textContent = transaction;
-            transactionList.appendChild(listItem);
-        }
+function updateTransactionHistory(transaction) {
+    const transactionList = document.getElementById("transactionList");
+    const listItem = document.createElement("li");
+    listItem.textContent = transaction;
+    transactionList.appendChild(listItem);
+}
 
-       
-        function transferMoney() {
-            times = 0
-            const transferAmount = parseFloat(document.getElementById("transferAmount").value);
-            var trnfrTo = parseFloat(document.getElementById("Norek").value);
-            while(times<3){
-                if (transferAmount != null && trnfrTo != null){
-                    if (isNaN(transferAmount) && isNaN(trnfrTo)) {
-                        alert("Please enter a number");
-                        return;
-                    }
-                    
-                    if (bankBalance >= transferAmount) {
-                        temp1 = prompt("Please enter your passcode to complete the transaction.")
-                        if(temp1 == currentPasscode){
-                            updateBalance();
-                            updateTransactionHistory(`Transferred $${transferAmount} to ${trnfrTo} `);
-                            bankBalance -= transferAmount;
-                            break
-                        }
-                        else{
-                            times += 1
-                            alert("WRONG PASSCODE")}
-                    } else {
-                        alert("Insufficient funds.");
-                        break
-                    }
-                }else{
-                    alert("check input")
-                }
+
+function transferMoney() {
+    const transferAmount = parseFloat(document.getElementById("transferAmount").value);
+    const trnfrTo = parseFloat(document.getElementById("Norek").value);
+
+    if (isNaN(transferAmount) || isNaN(trnfrTo)) {
+        alert("Please enter valid numbers.");
+    } else if (bankBalance < transferAmount) {
+        alert("Insufficient funds.");
+    } else {
+        const temp1 = prompt("Please enter your passcode to complete the transaction.");
+
+        if (temp1 !== null) { 
+            if (temp1 === currentPasscode) {
+                bankBalance -= transferAmount;
+                updateBalance();
+                updateTransactionHistory(`Transferred $${transferAmount} to ${trnfrTo}`);
+            } else {
+                alert("Wrong passcode. Transaction canceled.");
             }
-            if(times>2){
-                alert("TOO MANY ATEMPTS. LOGGING OUT")
-                location.replace("index.html")
-            }
+        } else {
+            alert("Transaction canceled.");
         }
+    }
+}
+
+
 
   
         document.getElementById("transferButton").addEventListener("click", transferMoney);
